f17c2b123c8e30cdd9b6fa241dd4a8567c193579
Nevardo Antonio Ospina Zuñiga <ospina_88@hotmail.com>
2023-01-22 15:46:55 -0500

    Actualizando Main

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -9,11 +9,12 @@ public class Test {
         Persona empleado = new Empleado();
         Scanner entrada = new Scanner(System.in);
         int menu;
-        System.out.println("Seleccione una de las siguientes opciones: \n"+
+        System.out.println("ssssssssssSeleccione una de las siguientes opciones: \n"+
                 "1. digitar informacion completa. \n" +
                 "2. Digitar solo nombre, apellido y edad. \n" +
                 "3. Digitar solo la informacion laboral. \n"+
                 "0. salir. ");
+
         menu = Integer.parseInt(entrada.nextLine());
 
         switch (menu){
